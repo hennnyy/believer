7401b9271d06dd32eb2d3c258f06d6747dc8f6f8
hyeeun <jhe1209@gmail.com>
2022-11-10 16:22:22 +0900

    메뉴 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,28 @@
 // 언어 별 디스플레이
 
 window.onload = function () {
-
   //언어별 디스플레이
 
-  // let koBtn = document.querySelector('.ko_btn');
-  // let koVer = document.querySelector('.ko_version');
-  // let enBtn = document.querySelector('.en_btn');
-  // let enVer = document.querySelector('.en_version');
+  let koBtn = document.querySelector('.ko_btn');
+  let koVer = document.querySelector('.ko_version');
+  let enBtn = document.querySelector('.en_btn');
+  let enVer = document.querySelector('.en_version');
 
-  // koBtn.addEventListener('click', function () {
-  //   enVer.style.display = "none";
-  //   koVer.style.display = "block";
-  //   console.log(enVer);
-  // })
+  koBtn.addEventListener('click', function () {
+    enVer.style.display = "none";
+    koVer.style.display = "block";
+    console.log(enVer);
+  })
 
-  // enBtn.addEventListener('click', function () {
-  //   koVer.style.display = "none";
-  //   enVer.style.display = "block";
-  // })
-
+  enBtn.addEventListener('click', function () {
+    koVer.style.display = "none";
+    enVer.style.display = "block";
+  })
   //고스트메뉴
 
   let menuBtn = document.querySelector('.menu_btn');
   let ghostList = document.querySelector('.ghost_menu');
-  let langBtn = document.querySelector('.lang_btn');
-  // let list = document.querySelectorAll('.menu_list');
+  let list = document.querySelectorAll('.menu_list');
   let subToggle = true;
 
   console.log(menuBtn);
@@ -38,8 +35,8 @@ window.onload = function () {
     subToggle = !subToggle;
   });
 
-  for (let i = 0; i < langBtn.length; i++) {
-    langBtn[i].addEventListener("click", function () {
+  for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener("click", function () {
       menuBtn.checked = false;
       ghostList.style.setProperty('height', '0px');
       subToggle = !subToggle;
