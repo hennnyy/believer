9fd81322dddead523f79d851b3a0a268c5e38970
hyeeun <jhe1209@gmail.com>
2022-11-10 15:52:40 +0900

    언어 스크립트, 메뉴 스크립트 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,30 @@
 // 언어 별 디스플레이
 
 window.onload = function () {
-  let langVer = document.querySelectorAll('.lang_ver');
-  let langBtn = document.querySelectorAll('.lang_btn');
-  let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
 
-  for (var i = 0; i < langBtn.length; i++) {
-    langBtn[i].addEventListener('click', function (e) {
-      e.preventDefault();
-      console.log('.lang_btn');
-      for (var j = 0; j < langBtn.length; j++) {
-        // 나머지 버튼 클래스 제거
-        langBtn[j].classList.remove('on');
+  //언어별 디스플레이
 
-        // 나머지 컨텐츠 display:none 처리
-        langVer[j].style.display = 'none';
-      }
+  // let koBtn = document.querySelector('.ko_btn');
+  // let koVer = document.querySelector('.ko_version');
+  // let enBtn = document.querySelector('.en_btn');
+  // let enVer = document.querySelector('.en_version');
 
-      // 버튼 관련 이벤트
-      this.parentNode.classList.add('on');
+  // koBtn.addEventListener('click', function () {
+  //   enVer.style.display = "none";
+  //   koVer.style.display = "block";
+  //   console.log(enVer);
+  // })
 
-      // 버튼 클릭시 컨텐츠 전환
-      activeCont = this.getAttribute('href');
-      document.querySelector(activeCont).style.display = 'block';
-    });
-  }
+  // enBtn.addEventListener('click', function () {
+  //   koVer.style.display = "none";
+  //   enVer.style.display = "block";
+  // })
 
   //고스트메뉴
 
   let menuBtn = document.querySelector('.menu_btn');
   let ghostList = document.querySelector('.ghost_menu');
+  let langBtn = document.querySelector('.lang_btn');
   // let list = document.querySelectorAll('.menu_list');
   let subToggle = true;
 
