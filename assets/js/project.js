1c95b037240e0136fc2309f2b6cd4dda86beeb95
jangjiheee <jihee.jang210@gmail.com>
2022-11-14 18:31:29 +0900

    project js 추가

---
+++ assets/js/project.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  var oneLine = document.querySelector(".right_one");
+  console.log(oneLine);
+
+  // if (topPos > )
+  // yOffset = oneLine.addEventListener("scroll", function () {
+    
+  // })\
+  const scrollPer = document.scrollTop();
+  console.log(scrollPer);
+}
